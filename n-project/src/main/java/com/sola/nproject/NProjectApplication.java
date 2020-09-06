276b6061c802ff1e243d996dc0847981a45158fa
solasj <solasj@qq.com>
2020-09-06 17:00:08 +0800

    [新建项目] 1. 新增 n-project 项目.

---
+++ n-project/src/main/java/com/sola/nproject/NProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sola.nproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NProjectApplication.class, args);
+	}
+
+}
